7d2042857d5572b8f45f61d2365641dfbc84b22b
voyaseryo <unanona@gmail.com>
2019-12-03 13:29:50 +0100

    version 1.0

---
+++ ejemplos/tema2/PerimetroCircunferencia.java
@@ -0,0 +1,13 @@
+public class PerimetroCircunferencia {
+
+    public static void main(String [] args){
+        final double Pi= 3.1415;
+        double radio=5;
+        double perimetro;
+
+        perimetro= 2 * Pi * radio;
+        System.out.println("El perimetro de la circunferencia de radio"+radio);
+        System.out.println("es");
+        System.out.println(perimetro);
+    }
+}
