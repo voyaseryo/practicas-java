c3b0fdffee509bb3f0552eab33e8d0f84123bff7
voyaseryo <58209250+voyaseryo@users.noreply.github.com>
2019-12-03 12:57:32 +0100

    primera version

---
+++ ejemplos/tema2/HolaMundo.java
@@ -0,0 +1,17 @@
+package tema2;
+
+/**
+ * 
+ */
+/**
+ * @author Jacobo
+ *
+ */
+
+
+public class HolaMundo{
+	
+	public static void main(String [] args) {
+		System.out.println("Hola Mundo !!");
+	}
+}
